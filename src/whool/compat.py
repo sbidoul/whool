87e44ee5664b23f6288aec956ab21afb7d9eb044
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2023-06-17 18:21:01 +0200

    Cosmetics

--- src/whool/compat.py
+++ src/whool/compat.py
@@ -1,6 +1,14 @@
 import sys
 
-__all__ = ["tomllib"]
+__all__ = [
+    "importlib_metadata",
+    "tomllib",
+]
+
+if sys.version_info < (3, 8):  # pragma: no cover (<PY38)
+    import importlib_metadata
+else:  # pragma: no cover (PY38+)
+    import importlib.metadata as importlib_metadata
 
 if sys.version_info < (3, 11):
     import tomli as tomllib
--- src/whool/version.py
+++ src/whool/version.py
@@ -1,8 +1,3 @@
-import sys
-
-if sys.version_info < (3, 8):  # pragma: no cover (<PY38)
-    import importlib_metadata
-else:  # pragma: no cover (PY38+)
-    import importlib.metadata as importlib_metadata
+from .compat import importlib_metadata
 
 version = importlib_metadata.version("whool")
